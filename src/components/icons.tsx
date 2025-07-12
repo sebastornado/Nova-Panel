55de6e31b4aed08d36284f9cda8d137146cc5a90
Sebastian Torres <sebastornado.12@gmail.com>
2025-07-12 22:16:36 +0000

    quiero que el logo de nova panel sea algo relacionado con soporte tecnol

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,13 +4,19 @@ export function NovaPanelLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 256"
       width="1.5em"
       height="1.5em"
+      viewBox="0 0 24 24"
       {...props}
     >
-      <path fill="hsl(var(--primary))" d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24Zm0 192a88 88 0 1 1 88-88a88.1 88.1 0 0 1-88 88Z" />
-      <path fill="hsl(var(--primary))" d="m164.38 77.62l-64 32a8 8 0 0 0 0 12.76l64 32A8 8 0 0 0 176 148V84a8 8 0 0 0-11.62-6.38ZM160 135.24l-42.33-21.17L160 92.76Z" />
+      <g fill="none" stroke="hsl(var(--primary))" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M12 21.5a9.5 9.5 0 1 0-9.5-9.5" />
+        <path d="M15.5 15.5L12 12l-1.5 1.5" />
+        <path d="M12 12l1.5 1.5" />
+        <path d="M12 21.5V17l-3.5-3.5" />
+        <path d="M8.5 13.5L7 12" />
+        <path d="M12 8.5a2.5 2.5 0 1 0-5 0a2.5 2.5 0 0 0 5 0" />
+      </g>
     </svg>
   );
 }
